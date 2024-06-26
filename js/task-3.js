0e218191af2b9adb0e24e2d48e643d6da970ea47
ArthurMykhailiuk <artur.myhajlyuk@volo.global>
2024-06-26 12:56:38 +0300

    add js-hw-08

--- js/task-3.js
+++
@@ -1,8 +0,0 @@
-const nameInput = document.querySelector('#name-input');
-const nameOutput = document.querySelector('#name-output');
-
-nameInput.addEventListener('input', () => {
-  const trimmedValue = nameInput.value.trim();
-  const greetingName = trimmedValue ? trimmedValue : 'Anonymous';
-  nameOutput.textContent = greetingName;
-});
